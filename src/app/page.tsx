aac54cb7e831caa746b24bd2109b3756ed1723a1
shotastage <shota.shimazu@magicalsoft.app>
2025-12-22 09:24:20 +0900

    Add unique keys to WorkCard components in works and writings sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -59,7 +59,7 @@ export default async function Home() {
           articles[0]?.contents
             ?.filter((article: ArticleContent) => article?.category?.name === 'works')
             .map((article: ArticleContent) => {
-              return <WorkCard cardTitle={article.title} href={article?.id} eyecatch={article.eyecatch?.url}></WorkCard>;
+              return <WorkCard key={article.id} cardTitle={article.title} href={article?.id} eyecatch={article.eyecatch?.url}></WorkCard>;
             })
         }
       </WorkCardsContainer>
@@ -71,7 +71,7 @@ export default async function Home() {
           articles[0]?.contents
             ?.filter((article: ArticleContent) => article?.category?.name === 'writings')
             .map((article: ArticleContent) => {
-              return <WorkCard cardTitle={article.title} href={article?.id} eyecatch={article.eyecatch?.url}></WorkCard>;
+              return <WorkCard key={article.id} cardTitle={article.title} href={article?.id} eyecatch={article.eyecatch?.url}></WorkCard>;
             })
         }
       </WorkCardsContainer>
